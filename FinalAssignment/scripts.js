b68643a3c69d59dd263d77aeef9a04ed1c364f9b
misaacs-winc <88142732+misaacs-winc@users.noreply.github.com>
2021-11-12 14:53:37 +0100

    add files
    Rework the final assignment

---
+++ FinalAssignment/scripts.js
@@ -0,0 +1,30 @@
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function ulLi() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+//function changebackground() {
+// document.body.style.backgroundColor = 'green';
+
+//}
+
+/*document.getElementById("blue").addEventListener("mouseover", "click", function () {
+  document.getElementById("blue").style.backgroundColor = "blue";
+});
+
+
+//body.addEventListener('click', function (e) { document.getElementById("body").style.backgroundColor = "body"; });
+
+document.getElementById("headnav").addEventListener("mouseout", function () {
+  document.getElementById("headnav").style.backgroundColor = "yellow";
+});
+
+document.getElementById("headnav").addEventListener("mouseout", function () {
+  document.getElementById("headnav").style.backgroundColor = "purple";
+});
+*/
